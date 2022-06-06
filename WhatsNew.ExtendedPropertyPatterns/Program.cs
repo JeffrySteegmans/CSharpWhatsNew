58f7604c23af0ad046c18a282e24ad78b7894119
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2022-06-06 11:57:41 +0200

    Extended Property Patterns

---
+++ WhatsNew.ExtendedPropertyPatterns/Program.cs
@@ -0,0 +1,24 @@
+﻿var person = new Person
+{
+    Name = "John Doe",
+    Address = new Address
+    {
+        Street = "Example street"
+    }
+};
+
+if (person is Person { Address.Street: "Example street" })
+{
+    Console.WriteLine(person.Name);
+}
+
+class Person
+{
+    public string Name { get; set; }
+    public Address Address { get; set; }
+}
+
+class Address
+{
+    public string Street { get; set; }
+}
