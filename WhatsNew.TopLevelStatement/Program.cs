b968ea891f8cb795c4469bda6b5256bec3c68538
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2022-06-06 09:39:58 +0200

    TopLevelStatements

---
+++ WhatsNew.TopLevelStatement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.WriteLine("Hello World!");
+
+//namespace WhatsNew.TopLevelStatement
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+//}
