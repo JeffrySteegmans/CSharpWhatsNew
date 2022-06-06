a48fbfdf83c1f099680daf6aefc33ebc6e9dbc0c
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2022-06-06 15:44:13 +0200

    Record structs

--- WhatsNew.RecordStructs/Program.cs
+++ WhatsNew.RecordStructs/Program.cs
@@ -1,5 +1,6 @@
-﻿
+﻿Console.WriteLine("");
+
 record Person(string FirstName, string LastName);
-record class Person(string FirstName, string LastName);
+//record class Person(string FirstName, string LastName);
 
 readonly record struct Point(double X, double Y, double Z);
