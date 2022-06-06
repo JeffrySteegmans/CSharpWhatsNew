793b09971a5a6d5669a29b049f1859b1e74f52c4
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2022-06-06 21:28:59 +0200

    Asynchronous streams

---
+++ WhatsNew.AsynchronousStreams/Program.cs
@@ -0,0 +1,14 @@
+﻿await foreach (var seasonTicketId in GetSeasonTicketIds())
+{
+    Console.WriteLine(seasonTicketId);
+}
+
+static async IAsyncEnumerable<Guid> GetSeasonTicketIds()
+{
+    for (int i = 0; i < 20; i++)
+    {
+        await Task.Delay(1000);
+
+        yield return Guid.NewGuid();
+    }
+}
