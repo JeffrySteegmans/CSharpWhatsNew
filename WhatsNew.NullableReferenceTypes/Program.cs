300d396bcd4c5b2e70f1658edc3a05df248d635a
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2022-06-06 15:44:24 +0200

    Nullable reference types

---
+++ WhatsNew.NullableReferenceTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+string nonNullableString = null;
+
+Console.WriteLine(nonNullableString);
+
+
+WriteMessage(null);
+
+void WriteMessage(string? message)
+{
+    if (IsNotNull(message))
+        Console.WriteLine(message.Length);
+}
+
+bool IsNotNull(string? value)
+{
+    return value is not null;
+}
